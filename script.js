9893f29a029a89f10f0b33b4e418aa8aad0b8f21
Kritika745 <yadavkritika745@gmail.com>
2023-10-24 18:13:22 +0530

    score count

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
 document.body.addEventListener('click', () => document.getElementById('gunshot').play());
 
-let bullet=6;
-let count=0;
+let count = 0;
+let bullet = 6;
 
-function shoot(bird){
+function shoot(bird) {
 
-            bird.style.animation='fall 2s linear 0s 1 forwards';
+  bird.style.animation = 'fall 2s linear 0s 1 forwards';
 
-            let dog = document.getElementById('dog');
-            dog.style.animation = 'dogmov 0.5s linear 0s 2 alternate';
+  let dog = document.getElementById('dog');
+  dog.style.animation = ''; 
+  void dog.offsetWidth;
+  dog.style.animation = 'dogmov 0.5s linear 0s 2 alternate';
+  
+  document.getElementById('duck-caught').play();
 
-            
+  count += 500;
+  bullet -= 1;
+
+  document.getElementById("currentscore").innerHTML = count;
+  document.getElementById("currentbullets").innerHTML = bullet;
 }
+
+
+
+
+
+
+
+
