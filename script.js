aeef162c7aafc764d5b0e92acc8649f2de6bd7f8
krishna <krishnabansal583@gmail.com>
2023-10-26 23:28:27 +0530

    making responsive

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 let count = 0;
 let bullet = 20;
 
+
+
 //bullet sound and bullet count
 document.body.addEventListener('click', () => {
-   document.getElementById('gunshot').play();
-
+  
   let bullet = parseInt(document.getElementById("currentbullets").innerHTML);
 
   if (bullet > 0) {
     bullet -= 1;
     document.getElementById("currentbullets").innerHTML = bullet;
   }
+  if(bullet){
+  document.getElementById('gunshot').play();
+}
 });
 
 
@@ -22,9 +26,9 @@ function shoot(bird) {
   dog.style.animation = '';
   void dog.offsetWidth;
   dog.style.animation = 'dogmov 0.5s linear 0s 2 alternate';
-
-  document.getElementById('duck-caught').play();
-
+  if(bullet){
+     document.getElementById('duck-caught').play();
+  }
   count += 500;
  
 
@@ -47,19 +51,37 @@ const birdElements = [
   document.getElementById("bird11"),
   document.getElementById("bird12"),
 ];
+let timeoutID; 
 
-function playBirdSoundWithDelay(){
-  setTimeout(() => {
-    document.getElementById("duck-flapping").play();
-
-  }, 1000);
+function playBirdSoundWithDelay() {
+  timeoutID = setTimeout(() => {
+    if (bullet > 0) {
+      document.getElementById("duck-flapping").play();
+    }
+  }, 100);
 }
+
+function stopBirdSound() {
+  clearTimeout(timeoutID);
+}
+
 // Add animationstart event listener to each bird element
 birdElements.forEach((birdElement) => {
-  birdElement.addEventListener("animationstart",playBirdSoundWithDelay);
+  birdElement.addEventListener("animationstart", playBirdSoundWithDelay);
 });
 
+// Check for bullet changes and update the sound accordingly
+function updateSound() {
+  if (bullet > 0) {
+    playBirdSoundWithDelay();
+  } else {
+    stopBirdSound();
+  }
+}
 
+// Assuming you have a way to update the value of 'bullet' in your code,
+// you can call updateSound() whenever 'bullet' changes to control the sound:
+updateSound();
 
 
 
